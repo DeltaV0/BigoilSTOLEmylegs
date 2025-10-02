05c6193fb025caf836336f8423543bfc12f98279
pressgoal <isaiahmnasby@gmail.com>
2025-10-01 22:28:29 -0500

    Basic Tank Movement
    Changed player to character controller. Added basic movement.
    Need to add gravity.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-    public float movespeed;
-
     public Transform orient;
 
     float horizIn;
@@ -13,7 +11,9 @@ public class Player : MonoBehaviour
 
     Vector3 moveDir;
 
-    public Rigidbody rb;
+    private CharacterController cc;
+    public float rotSpeed = 45;
+    public float moveSpeed = 2;
 
     public float sensX;
     public float sensY;
@@ -22,42 +22,32 @@ public class Player : MonoBehaviour
 
     public float xRot;
     public float yRot;
-    // Start is called before the first frame update
+    
     void Start()
     {
-         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousx = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mousy = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        float mousex = Input.GetAxisRaw("Mouse X") * sensX;
+        float mousey = Input.GetAxisRaw("Mouse Y") * sensY;
 
-        yRot += mousx;
-        xRot -= mousy;
+        yRot += mousex;
+        xRot -= mousey;
 
         xRot = Mathf.Clamp(xRot, -90f, 90f);
-        cam.rotation = Quaternion.Euler(xRot, yRot, 0);
-        orient.rotation = Quaternion.Euler(0, yRot, 0);
+        yRot = Mathf.Clamp(yRot, -90f, 90f);
+        cam.localRotation = Quaternion.Euler(xRot, yRot, 0f);
 
         horizIn = Input.GetAxisRaw("Horizontal");
         vertiIn = Input.GetAxisRaw("Vertical");
 
-        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-
-        if(flatVel.magnitude > 5){
-            Vector3 limitedVel = flatVel.normalized * movespeed;
-            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        moveDir = orient.forward * vertiIn + orient.right * horizIn;
-        rb.AddForce(moveDir.normalized * movespeed * 10f, ForceMode.Force);
-
-
+        
+        orient.Rotate(Vector3.up * horizIn * rotSpeed * Time.deltaTime * (vertiIn >= 0f ? 1f : -1f));
+        cc.Move(orient.forward * vertiIn * moveSpeed * Time.deltaTime);
     }
 }
