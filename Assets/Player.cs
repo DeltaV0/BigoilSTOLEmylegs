f0617dd0d6ada5c12418900505df3a7b074b390d
pressgoal <isaiahmnasby@gmail.com>
2025-10-01 22:47:54 -0500

    Added gravity
    Character now falls properly, and can't be influenced by player input while in the air.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -22,7 +22,13 @@ public class Player : MonoBehaviour
 
     public float xRot;
     public float yRot;
-    
+
+    public bool grounded = false;
+    public LayerMask groundMask;
+    public float groundDist = 0.01f;
+
+    public Vector3 velocity;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -33,6 +39,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //  Are we on the ground
+        Vector3 center = transform.position + cc.center;
+        Vector3 offset = transform.up * Mathf.Max(cc.height - 2f * cc.radius) / 2f;
+        grounded = Physics.CapsuleCast(center + offset, center - offset, cc.radius, Vector3.down, groundDist, groundMask);
+        if(grounded)
+        {
+            //  Even if we're on the ground, move down a little, just to account for discrepancies
+            velocity = Vector3.down * -0.1f;
+        }
+        else
+        {
+            velocity += Physics.gravity * Time.deltaTime;
+        }
+
+        //  Look stuff
         float mousex = Input.GetAxisRaw("Mouse X") * sensX;
         float mousey = Input.GetAxisRaw("Mouse Y") * sensY;
 
@@ -43,11 +64,16 @@ public class Player : MonoBehaviour
         yRot = Mathf.Clamp(yRot, -90f, 90f);
         cam.localRotation = Quaternion.Euler(xRot, yRot, 0f);
 
-        horizIn = Input.GetAxisRaw("Horizontal");
-        vertiIn = Input.GetAxisRaw("Vertical");
+        //  Movement stuff, can't move if not on ground because wheelchair
+        if (grounded)
+        {
+            horizIn = Input.GetAxisRaw("Horizontal");
+            vertiIn = Input.GetAxisRaw("Vertical");
 
-        
-        orient.Rotate(Vector3.up * horizIn * rotSpeed * Time.deltaTime * (vertiIn >= 0f ? 1f : -1f));
-        cc.Move(orient.forward * vertiIn * moveSpeed * Time.deltaTime);
+
+            orient.Rotate(Vector3.up * horizIn * rotSpeed * Time.deltaTime * (vertiIn >= 0f ? 1f : -1f));
+            velocity += orient.forward * vertiIn * moveSpeed;
+        }
+        cc.Move(velocity * Time.deltaTime);
     }
 }
