3121ae32c8fc89471eea71e14cbbd0179ee86105
Blurr507 <porgs4life507@gmail.com>
2025-10-08 20:01:44 -0700

    started inventory

---
+++ Assets/Blurr's Stuff/BlurrInventory.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BlurrInventory : MonoBehaviour
+{
+    public GameObject[] items;
+
+    public int keyCardLevel = 0;
+
+    public int ammoCount = 0;
+
+    //uhh im lowk tired and have no motivation rn soooo im gonna go to bed sry I will finish tmr
+
+    //gonna just make a hotbar that you can hold the gun/flashlight/audio tapes in, ammo to load into a separate gun script, and MAYBE batteries for the flashlight if yall want that
+
+    //keycardlevel is gonna be for upgrading the access you have to the map, that seems a lot easier that a bunch of different keys
+
+    }
+}
