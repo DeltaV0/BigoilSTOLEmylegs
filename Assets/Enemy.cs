30d9cfd9e43c2be9857e9eb5444286479d415b30
DeltaV <Tristantdm1@outlook.com>
2025-10-08 20:45:27 -0400

    a bit more

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -48,15 +48,16 @@ public class Enemy : MonoBehaviour
         //tipstarget[3].position = Vector3.Lerp(tipstarget[3].position, tipstargetPos[3], timer[0] / 40f);
 
         // Vector3.Lerp()
-
+        
         if (timer[0] <= 0)
         {
             //move leg one
             timer[0] = 40;
             RaycastHit hit;
-            Physics.Raycast(offsets[0].position, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
+            Physics.Raycast(offsets[0].position + agent.velocity * 0.5f, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
             tipstargetPos[0] = hit.point;
-            tipstarget[0].DOJump(tipstargetPos[0], 1f, 1, 0.25f);
+            tipstarget[0].DOJump(tipstargetPos[0], Vector3.Distance(tipstarget[0].position, tipstargetPos[0]) * 0.25f, 1, 0.25f);
+            //transform.DOShakeRotation(0.1f, 22.5f, 1, 22.5f, true);
 
         }
         else
@@ -69,9 +70,10 @@ public class Enemy : MonoBehaviour
             //move leg 2
             timer[2] = 40;
             RaycastHit hit;
-            Physics.Raycast(offsets[2].position, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
+            Physics.Raycast(offsets[2].position + agent.velocity * 0.5f, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
             tipstargetPos[2] = hit.point;
-            tipstarget[2].DOJump(tipstargetPos[2], 1f, 1, 0.25f);
+            tipstarget[2].DOJump(tipstargetPos[2], Vector3.Distance(tipstarget[2].position, tipstargetPos[2]) * 0.25f, 1, 0.25f);
+            //transform.DOShakeRotation(0.1f, 22.5f, 1, 22.5f, true);
         }
         else
         {
@@ -84,9 +86,10 @@ public class Enemy : MonoBehaviour
             //move leg 3
             timer[1] = 40;
             RaycastHit hit;
-            Physics.Raycast(offsets[1].position, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
+            Physics.Raycast(offsets[1].position + agent.velocity * 0.5f, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
             tipstargetPos[1] = hit.point;
-            tipstarget[1].DOJump(tipstargetPos[1], 1f, 1, 0.25f);
+            tipstarget[1].DOJump(tipstargetPos[1], Vector3.Distance(tipstarget[1].position, tipstargetPos[1]) * 0.25f, 1, 0.25f);
+           // transform.DOShakeRotation(0.1f, 22.5f, 1, 22.5f, true);
         }
         else
         {
@@ -98,9 +101,10 @@ public class Enemy : MonoBehaviour
             //move leg 4
             timer[3] = 40;
             RaycastHit hit;
-            Physics.Raycast(offsets[3].position, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
+            Physics.Raycast(offsets[3].position + agent.velocity * 0.5f, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
             tipstargetPos[3] = hit.point;
-            tipstarget[3].DOJump(tipstargetPos[3], 1f, 1, 0.25f);
+            tipstarget[3].DOJump(tipstargetPos[3], Vector3.Distance(tipstarget[3].position, tipstargetPos[3]) * 0.25f, 1, 0.25f);
+            //transform.DOShakeRotation(0.1f, 22.5f, 1, 22.5f, true);
         }
         else
         {
