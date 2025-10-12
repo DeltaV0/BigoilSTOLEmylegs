a82cc01dba5b23c7ce1c9ebd017cf477ffe10ee2
DeltaV <Tristantdm1@outlook.com>
2025-10-11 22:33:07 -0400

    slightly better AI

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -26,9 +26,20 @@ public class Enemy : MonoBehaviour
     public Transform[] eyes;
 
     public Rigidbody rb;
+
+    public bool pathAvailable;
+    public NavMeshPath navMeshPath;
+
+    public Transform patrolPoints;
+
+    public Transform playerLastSeen;
+
+    public int wait;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        navMeshPath = new NavMeshPath();
+        agent.SetDestination(transform.position);
         //agent.updatePosition = false;
         //offset timers
         timer[1] = 20;
@@ -39,8 +50,22 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //target player with ai
-        agent.SetDestination(player.position);
+        if (wait > 0)
+        {
+            wait--;
+        }
+        //more advanced system ig
+        pathAvailable = agent.CalculatePath(player.position, navMeshPath);
+        if (agent.Raycast(target.position) && navMeshPath.status == NavMeshPathStatus.PathComplete)
+        {
+            agent.SetDestination(player.position);
+            playerLastSeen = player.position;
+        }
+        else
+        {
+            agent.SetDestination(playerLastSeen.position);
+        }
+            
         transform.LookAt(transform.position + agent.velocity);
 
         head.DODynamicLookAt(player.position, 5f);
@@ -57,7 +82,7 @@ public class Enemy : MonoBehaviour
                 //move leg x
                 timer[i] = 80;
                 RaycastHit hit;
-                Physics.Raycast(offsets[i].position + agent.velocity * 1.25f, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
+                Physics.Raycast(offsets[i].position + (agent.velocity * 1.25f) * transform.localScale.z, Vector3.down, out hit, 100f, LayerMask.GetMask("Ground"));
                 tipstargetPos[i] = hit.point;
                 tipstarget[i].DOJump(tipstargetPos[i], Vector3.Distance(tipstarget[i].position, tipstargetPos[i]) * 0.2f, 1, 0.5f);
                 //transform.DOShakeRotation(0.1f, 22.5f, 1, 22.5f, true);
